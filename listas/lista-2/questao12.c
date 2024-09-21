98de3937dcc1987c2174ebef731577d53303f5b9
ythmxz <ytcarvalho14@gmail.com>
2024-09-21 01:44:23 -0300

    Adiciona a questão 12

--- listas/lista-2/questao12.c
+++ listas/lista-2/questao12.c
@@ -12,10 +12,50 @@ encontrem no meio.
 
 #include <stdio.h>
 
+void inverte(char *pinicio, char *pfinal, int tamanhoString);
+
 int main() {
 
+	int tamanhoString = 0;
+	char string[51];
 
+	printf("\nDigite um texto (50 caracteres):\n\n");
+	scanf("%50[^\n]%*c", string);
+
+	char *pinicio = string;
+	char *pfinal = string;
+
+	while ( *pfinal != '\0' ) {
+
+		tamanhoString++;
+		pfinal++;
+
+	}
+
+	pfinal--;
+
+	inverte(pinicio, pfinal, tamanhoString);
+
+	printf("%s", string);
 
 	return 0;
+
+}
+
+void inverte(char *pinicio, char *pfinal, int tamanhoString) {
+
+	char temp;
+	int i;
+
+	for ( i = 0; i < (tamanhoString / 2); i++ ) {
+
+		temp = *pinicio;
+		*pinicio = *pfinal;
+		*pfinal = temp;
+
+		pinicio++;
+		pfinal--;
+
+	}
 
 }
