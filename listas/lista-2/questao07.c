61661c815962ea3955d2ad9f1f04a3412731d953
ythmxz <ytcarvalho14@gmail.com>
2024-09-21 03:13:14 -0300

    Adiciona a questão 7

--- listas/lista-2/questao07.c
+++ listas/lista-2/questao07.c
@@ -18,10 +18,65 @@ para a direita em 2 posições, o resultado seria [4, 5, 1, 2, 3].
 
 #include <stdio.h>
 
+void mover(int *parray, int tamanhoArray, int posicoes);
+
 int main() {
 
+	int i;
+	int tamanhoArray;
+	int posicoes;
 
+	printf("\nInsira o tamanho do array: ");
+	scanf("%d", &tamanhoArray);
+
+	int array[tamanhoArray];
+
+	printf("\n");
+
+	for ( i = 0; i < tamanhoArray; i++ ) {
+
+		printf("Array [%d]: ", i);
+		scanf("%d", (array + i));
+
+	}
+
+	printf("\nEste foi o array criado:\n");
+
+	for ( i = 0; i < tamanhoArray; i++ )
+		printf("\nArray [%d] = %d", i, array[i]);
+
+	printf("\n\nQuantas posições deseja mover? ");
+	scanf("%d", &posicoes);
+
+	mover(array, tamanhoArray, posicoes);
+
+	printf("\nAqui está o resultado:\n");
+
+	for ( i = 0; i < tamanhoArray; i++ )
+		printf("\nArray [%d] = %d", i, array[i]);
+
+	printf("\n");
 
 	return 0;
+
+}
+
+void mover(int *parray, int tamanhoArray, int posicoes) {
+
+	int i;
+	int arrayTemp[tamanhoArray];
+
+	for ( i = 0; i < tamanhoArray; i++ )
+		arrayTemp[i] = *(parray + i);
+
+    for (i = 0; i < posicoes; i++)
+        arrayTemp[i] = parray[tamanhoArray - posicoes + i];
+
+    for (i = posicoes; i < tamanhoArray; i++)
+        arrayTemp[i] = parray[i - posicoes];
+
+    for (i = 0; i < tamanhoArray; i++) {
+        parray[i] = arrayTemp[i];
+    }
 
 }
