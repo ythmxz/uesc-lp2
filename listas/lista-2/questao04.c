ae59541f884c284a0d14572d477512f65ea79d06
ythmxz <ytcarvalho14@gmail.com>
2024-09-21 02:03:55 -0300

    Adiciona a questão 4

--- listas/lista-2/questao04.c
+++ listas/lista-2/questao04.c
@@ -12,10 +12,42 @@ que calcule a área. Passe o retângulo por referência.
 
 #include <stdio.h>
 
+double calculoArea(double largura, double altura);
+
 int main() {
 
+	double area;
 
+	struct medidas {
+
+		double largura;
+		double altura;
+
+	};
+
+	struct medidas retangulo;
+
+	printf("\nInsira a largura: ");
+	scanf("%lf", &retangulo.largura);
+
+	printf("\nInsira a altura: ");
+	scanf("%lf", &retangulo.altura);
+
+	area = calculoArea(retangulo.largura, retangulo.altura);
+
+	printf("\nA area de um retângulo %gx%g é %g!\n"
+			, retangulo.largura, retangulo.altura, area);
 
 	return 0;
+
+}
+
+double calculoArea(double largura, double altura) {
+
+	double area;
+
+	area = (largura * altura);
+
+	return area;
 
 }
