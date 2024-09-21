fd65c1d6305af49520940e8ceabdffd290c2d8ba
ythmxz <ytcarvalho14@gmail.com>
2024-09-21 01:44:11 -0300

    Adiciona a questão 3

--- listas/lista-2/questao03.c
+++ listas/lista-2/questao03.c
@@ -12,10 +12,36 @@ Escreva uma função para criar um Ponto e, em seguida, imprima seus valores usa
 
 #include <stdio.h>
 
+void coordenada(int *px, int *py);
+
 int main() {
 
+	struct coordenadas {
 
+		int x;
+		int y;
+
+	};
+
+	struct coordenadas ponto;
+
+	int *px = &ponto.x;
+	int *py = &ponto.y;
+
+	coordenada(px, py);
 
 	return 0;
+
+}
+
+void coordenada(int *px, int *py) {
+
+	printf("\nInsira o ponto X: ");
+	scanf("%d", px);
+
+	printf("\nInsira o ponto Y: ");
+	scanf("%d", py);
+
+	printf("\nCoordenadas: (%d, %d)\n", *px, *py);
 
 }
