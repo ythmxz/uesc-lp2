c93fa6cd0394a73301f6853aedb12d47448485fc
ythmxz <ytcarvalho14@gmail.com>
2024-09-21 00:14:00 -0300

    Adiciona a questão 1

--- listas/lista-2/questao01.c
+++ listas/lista-2/questao01.c
@@ -11,11 +11,30 @@ Escreva um programa que pegue o raio de um círculo como entrada, calcule a
 */
 
 #include <stdio.h>
+#include <math.h>
+
+void calculoArea(double *praio, double *parea);
 
 int main() {
 
+	double raio, area;
 
+	double *praio = &raio;
+	double *parea = &area;
+
+	printf("\nInsira o raio da circunferência: ");
+	scanf("%lf", praio);
+
+	calculoArea(praio, parea);
+
+	printf("\nA área de uma circunferência de raio %g é %g.\n", raio, area);
 
 	return 0;
+
+}
+
+void calculoArea(double *praio, double *parea) {
+
+	*parea = 3.14 * pow(*praio, 2);
 
 }
