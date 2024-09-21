1ceca22d3f1784bf8f4cd7b25801d212e9a56d6b
ythmxz <ytcarvalho14@gmail.com>
2024-09-21 00:14:11 -0300

    Adiciona a questão 2

--- listas/lista-2/questao02.c
+++ listas/lista-2/questao02.c
@@ -12,10 +12,70 @@ calcula a soma dos elementos da matriz usando ponteiros.
 
 #include <stdio.h>
 
+void soma(int *pmatrizA, int *pmatrizB, int *pmatrizC, int tamanhoMatriz);
+
 int main() {
 
+	int i;
+	int tamanhoMatriz;
 
+	printf("\nInsira o tamanho dos vetores: ");
+	scanf("%d", &tamanhoMatriz);
+
+	int matrizA[tamanhoMatriz];
+
+	printf("\n");
+
+	for ( i = 0; i < tamanhoMatriz; i++ ) {
+
+		printf("Matriz A [%d]: ", i);
+		scanf("%d", matrizA + i);
+
+	}
+
+	int matrizB[tamanhoMatriz];
+
+	printf("\n");
+
+	for ( i = 0; i < tamanhoMatriz; i++ ) {
+
+		printf("Matriz B [%d]: ", i);
+		scanf("%d", matrizB + i);
+
+	}
+
+	printf("\nOs valores são os seguintes:\n");
+
+	for ( i = 0; i < tamanhoMatriz; i++ )
+		printf("\nMatriz A [%d] = %d", i, matrizA[i]);
+
+	printf("\n");
+
+	for ( i = 0; i < tamanhoMatriz; i++ )
+		printf("\nMatriz B [%d] = %d", i, matrizB[i]);
+
+	printf("\n");
+
+	int matrizC[tamanhoMatriz];
+
+	printf("\nAgora vamos somar as matrizes...\n");
+
+	soma(matrizA, matrizB, matrizC, tamanhoMatriz);
+
+	for ( i = 0; i < tamanhoMatriz; i++ )
+		printf("\nMatriz C [%d] = %d", i, matrizC[i]);
+
+	printf("\n");
 
 	return 0;
+
+}
+
+void soma(int *pmatrizA, int *pmatrizB, int *pmatrizC, int tamanhoMatriz) {
+
+	int i;
+
+	for ( i = 0; i < tamanhoMatriz; i++ )
+		*(pmatrizC + i) = *(pmatrizA + i) + *(pmatrizB + i);
 
 }
