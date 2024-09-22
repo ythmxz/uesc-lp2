d3eca8c39919f6b51eefa24dbe2be62fedaffb38
ythmxz <ytcarvalho14@gmail.com>
2024-09-22 00:23:33 -0300

    Adiciona a questão 8

--- listas/lista-2/questao08.c
+++ listas/lista-2/questao08.c
@@ -14,7 +14,48 @@ Dica: Use um ponteiro para acompanhar o aluno com o maior CR.
 
 int main() {
 
+	int i;
+	int quantidade;
 
+	struct registro {
+
+		char nome[31];
+		int matricula;
+		double nota;
+
+	};
+
+	printf("\nDigite a quantidade de alunos: ");
+	scanf("%d", &quantidade);
+
+	struct registro aluno[quantidade];
+
+	for ( i = 0; i < quantidade; i++ ) {
+
+		printf("\nNome: ");
+		scanf(" %30[^\n]%*c", aluno[i].nome);
+
+		printf("Matrícula: ");
+		scanf("%d", &aluno[i].matricula);
+
+		printf("Nota: ");
+		scanf("%lf", &aluno[i].nota);
+
+	}
+
+	printf("\nEste é o registro:\n");
+
+	for ( i = 0; i < quantidade; i++ ) {
+
+		printf("\nAluno %d\n", (i + 1));
+
+		printf("\nNome: %s", aluno[i].nome);
+		printf("\nMatrícula: %d", aluno[i].matricula);
+		printf("\nNota: %g", aluno[i].nota);
+
+		printf("\n");
+
+	}
 
 	return 0;
 
