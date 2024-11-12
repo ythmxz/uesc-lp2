dfa4b9c5169414d937f373f48f2e579a7c5a5dec
Yuri Thomaz <ytcarvalho14@gmail.com>
2024-11-12 15:04:17 -0300

    Declara as funções em clientes

--- exercicios/clientes.c
+++ exercicios/clientes.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+typedef struct {
+
+	char nome[51];
+	int id;
+	double carteira;
+
+} registro;
+
+void abrirArquivo();
+void criarRegistro();
+void expandirRegistro();
+void reduzirRegistro();
+void mostrarRegistro();
+
 int main() {
 
 
@@ -7,3 +21,22 @@ int main() {
 	return 0;
 
 }
+
+void abrirArquivo() {
+
+	FILE *p_arquivo;
+
+	p_arquivo = fopen("registro.txt", "r+");
+
+	if ( p_arquivo == NULL )
+		p_arquivo = fopen("registro.txt", "w+");
+
+}
+
+void criarRegistro() {}
+
+void expandirRegistro() {}
+
+void reduzirRegistro() {}
+
+void mostrarRegistro() {}
