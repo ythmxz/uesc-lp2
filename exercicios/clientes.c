e1cf850c28aa469f88e797f62f3bdc74bc7aaadb
Yuri Thomaz <ytcarvalho14@gmail.com>
2024-11-19 14:46:20 -0300

    Define funções de criação do registro

--- exercicios/clientes.c
+++ exercicios/clientes.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+const int LIMITE_CLIENTES = 100;
+
 typedef struct {
 
 	char nome[51];
@@ -9,14 +11,37 @@ typedef struct {
 } registro;
 
 void abrirArquivo();
-void criarRegistro();
+void criarRegistro(registro *p_cliente, int tamanho);
 void expandirRegistro();
 void reduzirRegistro();
-void mostrarRegistro();
+void mostrarRegistro(registro *p_cliente, int tamanho);
 
 int main() {
 
+	int tamanho;
 
+ENTRADA_TAMANHO:
+
+	printf("\nDigite o tamanho inicial do registro: ");
+	scanf("%d", &tamanho);
+
+	while ( tamanho <= 0  || tamanho > LIMITE_CLIENTES) {
+
+		printf("\nTamanho inválido!\n");
+
+		goto ENTRADA_TAMANHO;
+
+	}
+
+	registro cliente[tamanho];
+
+	registro *p_cliente;
+
+	p_cliente = cliente;
+
+	criarRegistro(p_cliente, tamanho);
+
+	mostrarRegistro(cliente, tamanho);
 
 	return 0;
 
@@ -33,10 +58,41 @@ void abrirArquivo() {
 
 }
 
-void criarRegistro() {}
+void criarRegistro(registro *p_cliente, int tamanho) {
+
+	for ( int i = 0; i < tamanho; i++ ) {
+
+		printf("\nCliente %d\n", (i + 1));
+
+		printf("\nDigite o nome: ");
+		scanf("%50s", p_cliente[i].nome);
+
+		printf("Digite o ID: ");
+		scanf("%d", &p_cliente[i].id);
+
+		printf("Digite o saldo: ");
+		scanf("%lf", &p_cliente[i].carteira);
+
+	}
+
+}
 
 void expandirRegistro() {}
 
 void reduzirRegistro() {}
 
-void mostrarRegistro() {}
+void mostrarRegistro(registro *p_cliente, int tamanho) {
+
+	printf("\nRegistro atual:\n");
+
+	for ( int i = 0; i < tamanho; i++ ) {
+
+		printf("\nCliente %d\n", (i + 1));
+
+		printf("\nNome: %s\n", p_cliente[i].nome);
+		printf("ID: %02d\n", p_cliente[i].id);
+		printf("Carteira: %.2lf\n", p_cliente[i].carteira);
+
+	}
+
+}
